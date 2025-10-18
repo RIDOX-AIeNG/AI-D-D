b5bab41e9af695d28f07343e7f8880ea98c1df69
Ridwan <ayomideridox@gmail.com>
2025-10-18 15:40:08 +0100

    task 20

--- WEEK_6/TASK_FOLDER/Task_20/python_script/database_connection.py
+++ WEEK_6/TASK_FOLDER/Task_20/python_script/database_connection.py
@@ -3,4 +3,4 @@ from sqlalchemy import create_engine
 import pyscopg2
 
 # Create database connection
-engine = create_engine('postgresql://postgres:admin@localhost:5432/library_db')
+engine = create_engine('postgresql://postgres:admin@localhost:5433/library_db')
